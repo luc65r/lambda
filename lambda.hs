4470d22bb774a3743705906e2a667c4dde87ee16
luc65r <lucas@ransan.tk>
2020-06-18 15:14:35 +0200

    reductMax

--- lambda.hs
+++ lambda.hs
@@ -15,6 +15,12 @@ reduct (App a b) = App (reduct a) (reduct b)
 reduct (Abs a) = Abs $ reduct a
 reduct (Var a) = Var a
 
+reductMax :: Lambda -> Lambda
+reductMax x
+    | x == y = x
+    | otherwise = reductMax y
+    where y = reduct x
+
 substitute :: Lambda -> Int -> Lambda -> Lambda
 substitute (App a b) binders x = App (substitute a binders x) (substitute b binders x)
 substitute (Abs a) binders x = Abs $ substitute a (binders + 1) x
@@ -29,3 +35,12 @@ incFreeVar (Abs a) binders n = Abs $ incFreeVar a (binders + 1) n
 incFreeVar (Var a) binders n
     | a > binders = Var $ a + n
     | otherwise = Var a
+
+reductible :: Lambda -> Bool
+reductible (App (Abs _) _) = True
+reductible (App a b) = reductible a || reductible b
+reductible (Abs a) = reductible a
+reductible (Var _) = False
+
+reductMax' :: Lambda -> Lambda
+reductMax' = until (\x -> not $ reductible x) reduct
