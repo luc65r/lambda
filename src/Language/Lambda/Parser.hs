c51d1f2fd2c45b67773f4fc9ed2f2dca17b3bb1d
luc65r <lucas@ransan.tk>
2020-07-29 19:21:25 +0200

    Use Megaparsec instead of the custom parser

--- src/Language/Lambda/Parser.hs
+++ src/Language/Lambda/Parser.hs
@@ -6,112 +6,80 @@ Maintainer  : lucas@ransan.tk
 Stability   : stable
 Portability : portable
 
-Definition of the function 'parse', which parses a lambda expression from a string.
+Definition of the functions 'parse' and 'parseLambda',
+which parse a lambda expression from a string.
 -}
 
 {-# OPTIONS_GHC -Wno-unused-do-bind #-}
-{-# LANGUAGE LambdaCase, TupleSections #-}
 
 module Language.Lambda.Parser
-    ( parse
+    ( parseLambda
+    , parse
     ) where
 
 import Control.Applicative
-import Data.Char (isDigit)
+import Data.Void
+import qualified Text.Megaparsec as M
+import qualified Text.Megaparsec.Char as C
+import qualified Text.Megaparsec.Char.Lexer as L
 
 import Language.Lambda (Lambda(..))
 
-newtype Parser a = P { unP :: String -> (String, Either String a) }
+type Parser = M.Parsec Void String
 
-instance Functor Parser where
-    fmap f (P st) = P $ \s -> case st s of
-                                (r, Left a) -> (r, Left a)
-                                (r, Right a) -> (r, Right (f a))
+chainl1 :: Parser a -> Parser (a -> a -> a) -> Parser a
+chainl1 p op = do { x <- p; rest x }
+    where rest x = do
+            { f <- op
+            ; y <- p
+            ; rest (f x y)
+            } <|> return x
 
-instance Applicative Parser where
-    pure a = P (, Right a)
+sc :: Parser ()
+sc = L.space
+    C.space1
+    (L.skipLineComment ";")
+    empty
 
-    P ff <*> P xx = P $ \s0 -> case ff s0 of
-                                 (s1, Left a) -> (s1, Left a)
-                                 (s1, Right f) -> case xx s1 of
-                                                   (s2, Left a) -> (s2, Left a)
-                                                   (s2, Right x) -> (s2, Right (f x))
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme sc
 
-instance Alternative Parser where
-    empty = P (, Left "")
+symbol :: String -> Parser String
+symbol = L.symbol sc
 
-    (P f1) <|> (P f2) = P $ \s0 -> case f1 s0 of
-                                     (_, Left _) -> f2 s0
-                                     (s1, Right a) -> (s1, Right a)
+parens :: Parser Lambda
+parens = symbol "(" *> parseExpr <* symbol ")"
 
-    many (P f) = P go where
-        go s0 = case f s0 of
-                  (_, Left _) -> (s0, Right [])
-                  (s1, Right a) -> case go s1 of
-                                    (s2, Left b) -> (s2, Left b)
-                                    (s2, Right as) -> (s2, Right (a:as))
+parseExpr :: Parser Lambda
+parseExpr = parseApp <|> parseTerm
 
-    some (P f) = P $ \s0 -> case f s0 of
-                              (s1, Left a) -> (s1, Left a)
-                              (s1, Right a) ->
-                                  let (P fm) = many (P f)
-                                   in case fm s1 of
-                                        (s2, Left b) -> (s2, Left b)
-                                        (s2, Right as) -> (s2, Right (a:as))
-
-instance Monad Parser where
-    return = pure
-
-    (P x) >>= f = P $ \s -> case x s of
-                              (r, Left a) -> (r, Left a)
-                              (r, Right a) ->
-                                  let P fm = f a
-                                   in fm r
-
-
-parens :: Parser a -> Parser a
-parens p = do
-    satisfy (== '(')
-    many space
-    x <- p
-    many space
-    satisfy (== ')')
-    return x
-
-space :: Parser Char
-space = satisfy (== ' ')
-
-satisfy :: (Char -> Bool) -> Parser Char
-satisfy f = P $ \case
-                   [] -> ([], Left "Unexpected end")
-                   (c:cs)
-                       | f c -> (cs, Right c)
-                       | otherwise -> (cs, Left "Don't satisfy")
-
-chainl :: Parser a -> Parser (a -> a -> a) -> Parser a
-chainl p op = p >>= rest
-    where rest x = do f <- op
-                      y <- p
-                      rest (f x y)
-                      <|> return x
-
+parseTerm :: Parser Lambda
+parseTerm = M.choice
+    [ parseVar
+    , parseAbs
+    , parens
+    ]
 
 parseVar :: Parser Lambda
-parseVar = Var . read <$> some (satisfy isDigit)
+parseVar = Var <$> lexeme L.decimal
 
-parseAbs :: Parser Lambda -> Parser Lambda
-parseAbs p = Abs <$ satisfy (`elem` ['λ', '\\']) <*> p
+parseAbs :: Parser Lambda
+parseAbs = Abs <$> ((symbol "λ" <|> symbol "\\") *> parseExpr)
 
 parseApp :: Parser Lambda
-parseApp = chainl (parens parseApp
-    <|> parseAbs (many space >> parseApp)
-    <|> parseVar) (some space >> pure App)
+parseApp = chainl1 parseTerm $ pure App
 
 -- | Run the parser.
 --   Lambda can be represented by λ or \.
 --   The parser accepts extra parentheses and spaces.
+--   You will need to import 'Text.Megaparsec' to deal with the errors.
+parseLambda :: String -> Either (M.ParseErrorBundle String Void) Lambda
+parseLambda = M.parse (parseExpr <* M.eof) ""
+
+-- | Run the parser.
+--   The errors are transformed to 'String' for you,
+--   so you don't have to import 'Text.Megaparsec'.
 parse :: String -> Either String Lambda
-parse s = case unP parseApp s of
-            ([], Right a) -> Right a
-            (_, Left a) -> Left a
-            _ -> Left "Unmatched"
+parse s = case parseLambda s of
+            Left e -> Left $ M.errorBundlePretty e
+            Right l -> Right l
