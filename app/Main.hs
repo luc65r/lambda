75bb951676de2149382e53f36365b7c113927d94
luc65r <lucas@ransan.tk>
2020-07-29 12:40:15 +0200

    Add help to REPL

--- app/Main.hs
+++ app/Main.hs
@@ -8,12 +8,23 @@ repl :: IO ()
 repl = do
     input <- putStr "λ> " >> hFlush stdout >> getLine
     case input of
-      (':':c:_) -> case c of
-                      'q' -> exitSuccess
-                      _ -> putStrLn "Invalid command" >> repl
+      (':':c:_)
+          | c == 'q' -> exitSuccess
+          | c `elem` ['?', 'h'] -> putStrLn help >> repl
+          | otherwise -> putStrLn "Invalid command" >> repl
+
       _ -> putStrLn (case reductMax <$> parse input of
                        Right s -> show s
                        Left s -> "Invalid input: " ++ s) >> repl
+
+help :: String
+help = unlines
+    [ "  Commands available for the prompt:"
+    , ""
+    , "    <lambda expression>    reduct the expression"
+    , "    :h, :?                 display this help"
+    , "    :q                     exit this REPL"
+    ]
 
 main :: IO ()
 main = repl
